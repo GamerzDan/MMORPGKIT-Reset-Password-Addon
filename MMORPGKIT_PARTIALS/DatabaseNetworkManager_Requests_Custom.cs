f0964dd06dbbc8994a738635c83a197bfcc2e715
GamerzDan <contact@gamerzdan.com>
2023-08-06 21:26:18 +0530

    Update to v1.83d2

--- MMORPGKIT_PARTIALS/DatabaseNetworkManager_Requests_Custom.cs
+++ MMORPGKIT_PARTIALS/DatabaseNetworkManager_Requests_Custom.cs
@@ -5,12 +5,9 @@ namespace MultiplayerARPG.MMO
 {
     public partial class DatabaseNetworkManager
     {
-        public async UniTask<AsyncResponseData<EmptyMessage>> UpdateUserLoginAsync(CreateUserLoginReq request)
+        public async UniTask<DatabaseApiResult> UpdateUserLoginAsync(CreateUserLoginReq request)
         {
-            var result = await Client.SendRequestAsync<CreateUserLoginReq, EmptyMessage>(DatabaseRequestTypes.RequestUpdateUserLogin, request);
-            if (!result.IsSuccess)
-                Logging.LogError(nameof(DatabaseNetworkManager), $"Cannot {nameof(CreateUserLoginAsync)} status: {result.ResponseCode}");
-            return result;
+            return await SendRequest(request, DatabaseRequestTypes.RequestUpdateUserLogin, nameof(UpdateUserLoginAsync));
         }
     }
 }
--- MMORPGKIT_PARTIALS/IDatabaseClient_Custom.cs
+++ MMORPGKIT_PARTIALS/IDatabaseClient_Custom.cs
@@ -1,10 +1,30 @@
 using Cysharp.Threading.Tasks;
 using LiteNetLibManager;
+using Newtonsoft.Json;
+using System.Collections.Generic;
+using System.IO;
+#if !NET && !NETCOREAPP
+using UnityRestClient;
+#endif
 
 namespace MultiplayerARPG.MMO
 {
     public partial interface IDatabaseClient
     {
-        UniTask<AsyncResponseData<EmptyMessage>> UpdateUserLoginAsync(CreateUserLoginReq request);
+        UniTask<DatabaseApiResult> UpdateUserLoginAsync(CreateUserLoginReq request);
     }
-}
+
+
+    //IMPLEMENT METHOD FOR THE RESTDATABASECLIENT CLASS HERE
+    public partial class RestDatabaseClient : RestClient, IDatabaseClient
+    {
+        public async UniTask<DatabaseApiResult> UpdateUserLoginAsync(CreateUserLoginReq request)
+        {
+            return await SendRequest(request, GetUrl(apiUrl, DatabaseApiPath.UpdateUserLoginAsync), nameof(UpdateUserLoginAsync));
+        }
+    }
+    public partial class DatabaseApiPath
+    {
+        public const string UpdateUserLoginAsync = "update-user-login";
+    }
+ }
